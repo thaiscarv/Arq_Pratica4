31031a070f3ac4110bf7c18765e3205f5e0179b9
Thaís Carvalho <111368656+thaiscarv@users.noreply.github.com>
2023-04-19 21:52:46 -0300

    Add files via upload

---
+++ cinema-microservice/movies-microservice/src/index.test.js
@@ -0,0 +1,5 @@
+require("dotenv-safe").config();
+require("./config/mongodb.test").runTests();
+require("dotenv-safe").config();
+require("./config/mongodb.test").runTests();
+require("./repository/repository.test").runTests();
